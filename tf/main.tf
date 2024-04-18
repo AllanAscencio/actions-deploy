be7368f82edb3ecc6b89cb7fd2715a10668e7bb8
Allan Ascencio <allan.ascencio@gmail.com>
2024-04-17 21:09:24 -0400

    first commit

--- tf/main.tf
+++ tf/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0d7a109bf30624c99" # you may need to update this
+  ami           = "ami-09b90e09742640522" # you may need to update this
   instance_type = "t2.micro"
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-  key_name = "example" # update this
+  key_name = "terraform_keys" # update this
   user_data = <<-EOF
   #!/bin/bash
   sudo yum update -y
